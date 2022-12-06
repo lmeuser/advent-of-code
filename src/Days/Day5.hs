428095f4139718eed0f0af2d4b8f9c49ad22a58d
Lukas Meuser <lmeuser@users.noreply.github.com>
2022-12-06 14:23:20 +0100

    improve string display

--- src/Days/Day5.hs
+++ src/Days/Day5.hs
@@ -5,7 +5,7 @@ import Text.Megaparsec
 import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer (decimal)
 
-import Shared (runSolution)
+import Shared (runSolution, DisplayString (DisplayString))
 
 
 parser = (,) <$> (crateSection <* labelSection) <*> moveSection
@@ -25,7 +25,7 @@ applyMove transform stacks (n, from, to) = replace (from - 1) src' $ replace (to
 
 calculate transform (stacks, moves) = map head $ foldl (applyMove transform) stacks moves
 
-solve1 = calculate reverse
-solve2 = calculate id
+solve1 = DisplayString . calculate reverse
+solve2 = DisplayString . calculate id
 
 solution = runSolution parser solve1 solve2
--- src/Shared.hs
+++ src/Shared.hs
@@ -6,6 +6,11 @@ import Text.Megaparsec ( Parsec, runParser )
 
 type Parser = Parsec Void String
 
+newtype DisplayString = DisplayString String
+
+instance Show DisplayString where
+    show (DisplayString s) = s
+
 runSolution :: (Show b1, Show b2) => Parser a -> (a -> b1) -> (a -> b2) -> Int -> IO ()
 runSolution parser solve1 solve2 day = do
     let dayStr = show day
