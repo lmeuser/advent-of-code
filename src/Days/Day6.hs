33be32f432d79a4a77ce76b3de2d24fe0d90252a
Lukas Meuser <lmeuser@users.noreply.github.com>
2022-12-06 12:47:41 +0100

    tighten up day 6 code

--- src/Days/Day6.hs
+++ src/Days/Day6.hs
@@ -1,6 +1,6 @@
 module Days.Day6 where
 
-import Data.List (elemIndex, group, sort, tails)
+import Data.List (findIndex, nub, tails)
 import Data.Maybe (fromJust)
 import Text.Megaparsec
 import Text.Megaparsec.Char
@@ -9,8 +9,8 @@ import Shared (runSolution)
 
 parser = many letterChar
 
-solveFor n = (+n) . fromJust . (True `elemIndex`) . map sop . tails
-  where sop = (== n) . length . group . sort . take n
+solveFor n = (+n) . fromJust . findIndex (match . take n) . tails
+  where match xs = nub xs == xs
 
 solve1 = solveFor 4
 solve2 = solveFor 14
