1c1b9de59d9079c461a069cb30dc05db3dcf88e7
Lukas Meuser <lmeuser@users.noreply.github.com>
2023-10-21 20:53:07 +0200

    additional scaffolding for multi-year support

--- src/Main.hs
+++ src/Main.hs
@@ -1,53 +1,35 @@
-import System.Environment (getArgs)
-import Text.Read (readMaybe)
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# OPTIONS_GHC -fno-cse #-}
 
-import qualified Solutions.Year2022.Day1
-import qualified Solutions.Year2022.Day2
-import qualified Solutions.Year2022.Day3
-import qualified Solutions.Year2022.Day4
-import qualified Solutions.Year2022.Day5
-import qualified Solutions.Year2022.Day6
-import qualified Solutions.Year2022.Day7
-import qualified Solutions.Year2022.Day8
-import qualified Solutions.Year2022.Day9
-import qualified Solutions.Year2022.Day10
-import qualified Solutions.Year2022.Day11
-import qualified Solutions.Year2022.Day12
-import qualified Solutions.Year2022.Day13
-import qualified Solutions.Year2022.Day14
-import qualified Solutions.Year2022.Day15
--- import qualified Solutions.Year2022.Day16
+import System.Console.CmdArgs
 
-days = [ Solutions.Year2022.Day1.solution
-       , Solutions.Year2022.Day2.solution
-       , Solutions.Year2022.Day3.solution
-       , Solutions.Year2022.Day4.solution
-       , Solutions.Year2022.Day5.solution
-       , Solutions.Year2022.Day6.solution
-       , Solutions.Year2022.Day7.solution
-       , Solutions.Year2022.Day8.solution
-       , Solutions.Year2022.Day9.solution
-       , Solutions.Year2022.Day10.solution
-       , Solutions.Year2022.Day11.solution
-       , Solutions.Year2022.Day12.solution
-       , Solutions.Year2022.Day13.solution
-       , Solutions.Year2022.Day14.solution
-       , Solutions.Year2022.Day15.solution
-    --    , Solutions.Day16.solution
-       ]
+import Solutions.All (years)
 
-runDay day = (days !! (day - 1)) day
+runDay year day = ((years !! (year - 2015)) !! (day - 1)) day
+runYear year = mapM_ (runDay year) [1.. length (years !! (year - 2015))]
+runAll = mapM_ runYear [2015..2022]
+
+
+data Arguments = Arguments {
+    year :: Maybe Int
+  , day :: Maybe Int
+  }
+  deriving (Data, Typeable, Show)
+
+arguments = Arguments { year = def, day = def }
+
+validYear year = year >= 2022 && year <= 2022
+validDay year day = validYear year && day >= 1 && day <= length (years !! (year - 2015))
 
 main :: IO ()
 main = do
-    args <- getArgs
-    let existingDays = [1..length days]
-    if null args then
-         mapM_ runDay existingDays
-    else
-        case readMaybe . head $ args of
-            Nothing -> putStrLn "You need to provide either no arguments, or a day number"
-            Just x -> if x `elem` existingDays then
-                          runDay x
-                      else
-                          putStrLn ("Day " ++ show x ++ " does not exist")
+    args <- cmdArgs arguments
+    case (year args, day args) of
+        (Nothing, Just _) -> putStrLn "You need to provide a year if you provide a day"
+        (Just year, Nothing) -> if validYear year
+                                then runYear year
+                                else putStrLn "invalid year"
+        (Just year, Just day) -> if validDay year day
+                                 then runDay year day
+                                 else putStrLn "invalid year/day"
+        (Nothing, Nothing) -> runAll
---
+++ src/Solutions/All.hs
@@ -0,0 +1,46 @@
+module Solutions.All where
+
+import qualified Solutions.Year2022.Day1
+import qualified Solutions.Year2022.Day2
+import qualified Solutions.Year2022.Day3
+import qualified Solutions.Year2022.Day4
+import qualified Solutions.Year2022.Day5
+import qualified Solutions.Year2022.Day6
+import qualified Solutions.Year2022.Day7
+import qualified Solutions.Year2022.Day8
+import qualified Solutions.Year2022.Day9
+import qualified Solutions.Year2022.Day10
+import qualified Solutions.Year2022.Day11
+import qualified Solutions.Year2022.Day12
+import qualified Solutions.Year2022.Day13
+import qualified Solutions.Year2022.Day14
+import qualified Solutions.Year2022.Day15
+-- import qualified Solutions.Year2022.Day16
+
+years = [
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [
+    Solutions.Year2022.Day1.solution
+    , Solutions.Year2022.Day2.solution
+    , Solutions.Year2022.Day3.solution
+    , Solutions.Year2022.Day4.solution
+    , Solutions.Year2022.Day5.solution
+    , Solutions.Year2022.Day6.solution
+    , Solutions.Year2022.Day7.solution
+    , Solutions.Year2022.Day8.solution
+    , Solutions.Year2022.Day9.solution
+    , Solutions.Year2022.Day10.solution
+    , Solutions.Year2022.Day11.solution
+    , Solutions.Year2022.Day12.solution
+    , Solutions.Year2022.Day13.solution
+    , Solutions.Year2022.Day14.solution
+    , Solutions.Year2022.Day15.solution
+    -- , Solutions.Year2022.Day16.solution
+  ]
+  ]
