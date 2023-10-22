f620a2332f09b0683d9176eb35b155d3b26b8104
Lukas Meuser <lmeuser@users.noreply.github.com>
2023-10-22 19:55:02 +0200

    2015-04

---
+++ src/Solutions/Year2015/Day4.hs
@@ -0,0 +1,27 @@
+module Solutions.Year2015.Day4 where
+
+import qualified Crypto.Hash.MD5 as MD5
+import Data.ByteString.Base16 (encode)
+import qualified Data.ByteString.Char8 as BS
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
+import Shared
+
+parser :: Parser String
+parser = many lowerChar
+
+
+md5StartsWith :: BS.ByteString -> BS.ByteString -> Bool
+md5StartsWith cmp input = BS.take (BS.length cmp) (encode . MD5.hash $ input) == cmp
+
+solveFor :: String -> String -> Int
+solveFor cmp input = head . filter (md5StartsWith (BS.pack cmp) . BS.pack . (input ++) . show) $ [1..]
+
+solve1 :: String -> Int
+solve1 = solveFor "00000"
+
+solve2 :: String -> Int
+solve2 = solveFor "000000"
+
+solution = runSolution parser solve1 solve2
