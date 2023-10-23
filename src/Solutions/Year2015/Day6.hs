7b1241c3f6c5047e51a01e12dff04944fb98978e
Lukas Meuser <lmeuser@users.noreply.github.com>
2023-10-23 22:53:25 +0200

    much nicer solution for 2015-06

--- src/Solutions/Year2015/Day6.hs
+++ src/Solutions/Year2015/Day6.hs
@@ -1,52 +1,38 @@
 module Solutions.Year2015.Day6 where
 
-import qualified Data.Map as Map
+import Data.Array
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer
-
 import Shared
 
+
 type Point = (Int, Int)
-
-data Operation = On | Off | Toggle deriving Show
-
-data Instruction = Instruction Operation Point Point deriving Show
+data Operation = On | Off | Toggle
+data Instruction = Instruction Operation Point Point
 
 
 parser :: Parser [Instruction]
 parser = sepBy instruction newline
     where instruction = Instruction <$> operation <* char ' ' <*> point <* string " through " <*> point
           operation = (On <$ string "turn on") <|> (Off <$ string "turn off") <|> (Toggle <$ string "toggle")
-          point = (,) <$> (decimal <* char ',') <*> decimal
+          point = (,) <$> decimal <* char ',' <*> decimal
 
 
--- this is horribly inefficient and slow, I need to refactor this to use an actual array
+solveWith :: (Operation -> Int -> Int) -> [Instruction] -> Int
+solveWith apply = sum . accumArray (flip apply) 0 ((0, 0), (999, 999)) . (>>= expand)
+    where expand (Instruction op start end) = [(index, op) | index <- range (start, end)]
 
-allPoints :: Point -> Point -> [Point]
-allPoints (minX, minY) (maxX, maxY) = [(x, y) | x <- [minX..maxX], y <- [minY..maxY]]
+apply1 :: Operation -> Int -> Int
+apply1 On = const 1
+apply1 Off = const 0
+apply1 Toggle = (+1) . (* (-1))
 
-applyInstructions :: (Operation -> a -> a -> a) -> (Operation -> a) -> [Instruction] -> Map.Map (Int, Int) a
-applyInstructions apply dflt = foldl (\m (Instruction op start end) -> applyToPoints op m (allPoints start end)) Map.empty
-    where applyToPoints op = foldl (\m' k -> Map.insertWith (apply op) k (dflt op) m')
-
-solve1 :: [Instruction] -> Int
-solve1 = Map.size . Map.filter id . applyInstructions apply dflt
-    where apply On _ _ = True
-          apply Off _ _ = False
-          apply Toggle _ old = not old
-          dflt Off = False
-          dflt _ = True
-
-solve2 :: [Instruction] -> Int
-solve2 = sum . applyInstructions apply dflt
-    where apply On _ old = old + 1
-          apply Off _ old = max (old - 1) 0
-          apply Toggle _ old = old + 2
-          dflt On = 1
-          dflt Off = 0
-          dflt Toggle = 2
+apply2 :: Operation -> Int -> Int
+apply2 On = (+1)
+apply2 Off = max 0 . subtract 1
+apply2 Toggle = (+2)
 
 
 solution :: SolutionRunner
-solution = runSolution parser solve1 solve2
+solution = runSolution parser (solveWith apply1) (solveWith apply2)
