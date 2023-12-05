ebcfbfaa920ab0f06f4338a7099e054347679528
Lukas Meuser <lmeuser@users.noreply.github.com>
2023-12-06 00:28:17 +0100

    use range library, makes the code much nicer

--- src/Solutions/Year2023/Day5.hs
+++ src/Solutions/Year2023/Day5.hs
@@ -1,54 +1,32 @@
 module Solutions.Year2023.Day5 where
 
 import Control.Monad (void)
-import Data.List (sortOn, sort)
 import Data.List.Split (chunksOf)
-import Data.Maybe (maybeToList, isNothing)
+import Data.Range
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer
 
 import Shared
 
-parser = (,) <$> seeds <* doubleNew <*> sepBy mapping newline
+parser = (,) <$> seeds <* newline <* newline <*> sepBy mapping newline
   where seeds = string "seeds: " *> sepBy decimal (char ' ')
-        doubleNew = void (newline *> newline)
-        mapping = sortOn (fst . fst) <$> (mappingHeader *> sepEndBy mappingLine newline)
+        mapping = mappingHeader *> sepEndBy mappingLine newline
         mappingHeader = void <$> many (lowerChar <|> char '-') <* string " map:" <* newline
-        mappingLine = buildRangeLen <$> (decimal <* char ' ') <*> (decimal <* char ' ') <*> decimal
-        buildRangeLen dst src len = ((src, src + len - 1), dst)
+        mappingLine = buildRangeOffset <$> (decimal <* char ' ') <*> (decimal <* char ' ') <*> decimal
+        buildRangeOffset dst src len = (src +=* (src + len), dst - src)
 
-overlap r1@(s1, e1) (s2, e2)
-  | e1 < s2 = (Just r1, Nothing, Nothing)
-  | s1 > e2 = (Nothing, Nothing, Just r1)
-  | otherwise = let pre = if s1 < s2
-                          then Just (s1, s2 - 1)
-                          else Nothing
-                    post = if e1 > e2
-                           then Just (e2 + 1, e1)
-                           else Nothing
-                    match = Just (max s1 s2, min e1 e2)
-                in (pre, match, post)
+applyMappingPart seeds (src, offset) = map (fmap (+ offset)) (intersection seeds [src])
 
-applyMapping seeds = sort . helper seeds
-  where helper ss [] = ss
-        helper ss@(s:sr) ms@((src@(start, _), dst):mr)
-          | isNothing pre && isNothing match = helper ss mr
-          | otherwise = pre' ++ match' ++ helper left ms
-          where (pre, match, post) = overlap s src
-                dist = dst - start
-                move (a, b) =  (a + dist, b + dist)
-                pre' = maybeToList pre
-                match' = map move . maybeToList $ match
-                left = maybeToList post ++ sr
-        helper [] _ = []
+applyMapping seeds m = shifted `union` unshifted
+  where shifted = concatMap (applyMappingPart seeds) m
+        unshifted = difference seeds (map fst m)
 
-applyMappings mappings seeds = foldl applyMapping seeds mappings
+solve f (seeds, mappings) = minimum . map lowerBound . foldl applyMapping (f seeds) $ mappings
+  where lowerBound (SpanRange (Bound x Inclusive) _) = x
+        lowerBound (SingletonRange x) = x
 
-solve f (seeds, mappings) = head . map fst . applyMappings mappings . sort . f $ seeds
-
-solve1 = solve (map (\x -> (x, x)))
-
-solve2 = solve (map (\[a, b] -> (a, a + b - 1)) . chunksOf 2)
+solve1 = solve (map SingletonRange)
+solve2 = solve (map (\[a, b] -> a +=* (a + b)) . chunksOf 2)
 
 solution = runSolution parser solve1 solve2
