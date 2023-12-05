e76aaf3470586b9d38e02038f5c4f7c40df9a9f1
Lukas Meuser <lmeuser@users.noreply.github.com>
2023-12-05 14:31:19 +0100

    2023-05 part 1, WIP part 2

--- src/Solutions/Year2023/Day5.hs
+++ src/Solutions/Year2023/Day5.hs
@@ -8,26 +8,35 @@ import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer
 
 import Shared
+import Data.List.Split (chunksOf)
 
 
-newtype Mapping = Mapping [(Int, Int, Int)]
+newtype Mapping = Mapping [(Int, Int, Int)] deriving Show
 
--- buildMapping = Mapping . sortOn fst3
---   where fst3 (x, _, _) = x
-
--- applyMapping (Mapping m) n = helper n m
---   where helper n [] = n
---         helper n ((dst, src, len):xs) = if n >= src
---                                         then if n < src + len
---                                              then n - src + dst
---                                              else n
---                                         else 
+buildMapping = Mapping . sortOn snd3
+  where snd3 (_, x, _) = x
 
 parser = (,) <$> seeds <* doubleNew <*> sepBy mapping newline
   where seeds = string "seeds: " *> sepBy decimal (char ' ')
         doubleNew = () <$ (newline *> newline)
-        mapping = mappingHeader *> sepEndBy mappingLine newline
+        mapping = buildMapping <$> (mappingHeader *> sepEndBy mappingLine newline)
         mappingHeader = (,) <$> (many lowerChar <* string "-to-") <*> (many lowerChar <* string " map:") <* newline
         mappingLine = (,,) <$> (decimal <* char ' ') <*> (decimal <* char ' ') <*> decimal
 
-solution = runSolution parser (const ()) id
+applyMapping n (Mapping m) = helper n m
+  where helper n [] = n
+        helper n ((dst, src, len):xs)
+          | n >= src && n < src + len = n - src + dst
+          | n > src = helper n xs
+          | otherwise = n
+
+applyMappings mappings seed = foldl applyMapping seed mappings
+
+solve seeds mappings = minimum . map (applyMappings mappings) $ seeds
+
+solve1 (seeds, mappings) = solve seeds mappings
+
+solve2 (seeds, mappings) = solve seeds' mappings
+  where seeds' = concatMap (\[a, b] -> [a..a+b-1]) . chunksOf 2 $ seeds
+
+solution = runSolution parser solve1 solve2
