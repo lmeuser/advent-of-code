57e36f04346415c5b0088590bed8ce2868121735
Lukas Meuser <lmeuser@users.noreply.github.com>
2023-12-08 17:38:46 +0100

    more efficient 2023-06

--- src/Solutions/Year2023/Day6.hs
+++ src/Solutions/Year2023/Day6.hs
@@ -11,14 +11,13 @@ parser = (,) <$> times <*> distances
           distances = line "Distance:"
           line start = string start *> space1 *> sepEndBy decimal space1
 
-solveRace (time, target) = length . filter (> target) . distances $ time
-    where distances time = map (distance time) [1..time-1]
-          distance time press = (time - press) * press
+solveRace (time, target) = floor ((time / 2) + root) - ceiling ((time / 2) - root) + 1
+    where root = sqrt ((time ^ 2)/4 - target)
 
 solve1 = product . map solveRace . uncurry zip
 
 solve2 = solveRace . fixParse
     where fixParse (times, dists) = (fix times, fix dists)
-          fix = read . concatMap show
+          fix = read . concatMap (show . truncate)
 
 solution = runSolution parser solve1 solve2
