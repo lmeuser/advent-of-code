493a76de88d2e5df38c431f20095caeed561b4ff
Lukas Meuser <lukas.meuser@gmail.com>
2023-12-17 11:39:52 +0100

    new solution for 2023-12; includes part 2 this time

--- src/Solutions/Year2023/Day12.hs
+++ src/Solutions/Year2023/Day12.hs
@@ -1,40 +1,36 @@
-{-# LANGUAGE TupleSections #-}
 module Solutions.Year2023.Day12 where
 
+import Control.Parallel.Strategies
+import Data.List (intercalate)
+import Data.MemoTrie (memo2)
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer
 
 import Shared
-import Data.List (group)
-
-options 0 = [""]
-options n = let r = options (n - 1)
-            in map ('#':) r ++ map ('.':) r
-
-replace [] _ = []
-replace s [] = s
-replace (s:ss) os@(o:or)
-  | s == '?' = o:replace ss or
-  | otherwise = s:replace ss os
-
-solve123 (s, sol) = length . filter (== sol) $ ls
-  where os :: [[Char]]
-        os = options (length . filter (== '?') $ s)
-        fils :: [[Char]]
-        fils = map (replace s) os
-        brokenParts :: [[[Char]]]
-        brokenParts = map (filter ((== '#') . head) . group) $ fils
-        ls :: [[Int]]
-        ls = map (map length) brokenParts
-
-
--- length . filter (== [3,2,1]) $ map (map length . filter ((== '#') . head) . group) (map (replace "?###????????") (options 12))
-
-solve1 = sum . map solve123
 
 parser = sepBy line newline
   where line = (,) <$> some spring <* hspace <*> sepBy decimal (char ',')
         spring = char '.' <|> char '#' <|> char '?'
 
-solution = runSolution parser (const ()) solve1
+expand n (a, b) = (expand1 n "?" a, expand1 n [] b)
+  where expand1 n x = intercalate x . replicate n
+
+calc = memo2 calc'
+
+calc' [] ls = if null ls then 1 else 0
+calc' ss [] = if '#' `notElem` ss then 1 else 0
+calc' ss (l:ls) = let ss' = dropWhile (== '.') ss
+                      (start, rest) = splitAt l ss'
+                      p1 = if length start == l && '.' `notElem` start && (null rest || head rest /= '#')
+                           then calc (drop 1 rest) ls
+                           else 0
+                      p2 = if not (null ss') && head ss' /= '#'
+                           then calc (tail ss') (l:ls)
+                           else 0
+                  in p1 + p2
+
+solve :: Int -> [(String, [Int])] -> Int
+solve n = sum . parMap rdeepseq (uncurry calc . expand n)
+
+solution = runSolution parser (solve 1) (solve 5)
